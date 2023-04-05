dde13579c0794252db3ad729ed1976672ba1c69a
Charlie Crowe <charlietcrowe@gmail.com>
2023-04-05 16:13:47 +0000

    .

--- Model.py
+++ Model.py
@@ -9,13 +9,13 @@ from dataclasses import dataclass
 
 # hyperparameters
 batch_size = 8 # how many independent sequences will we process in parallel?
-block_size = 16
+block_size = 32
 max_iters = 5000
 eval_interval = 100
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 64
+n_embd = 128
 n_head = 4
 n_layer = 4
 dropout = 0.2
